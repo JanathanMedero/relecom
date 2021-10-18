487627358d9e1eea148821692fb414e279d56c71
JanathanMedero <medero.janathan@gmail.com>
2021-10-18 12:25:53 -0500

    First component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,60 @@
+<header>
+	<!-- Header Start -->
+	<div class="header-area header-transparent">
+		<div class="main-header ">
+
+			<div class="header-bottom header-sticky">
+				<div class="container-fluid">
+					<div class="row align-items-center">
+						<!-- Logo -->
+						<div class="col-xl-2 col-lg-2 col-md-1">
+							<div class="logo">
+								<!-- logo-1 -->
+								<a href="{{ route('home') }}" class="big-logo"><img src="assets/img/logo.png" alt=""></a>
+								<!-- logo-2 -->
+								<a href="index.html" class="small-logo"><img src="assets/img/logo/loder-logo.png" alt=""></a>
+							</div>
+						</div>
+						<div class="col-xl-8 col-lg-8 col-md-8">
+							<!-- Main-menu -->
+							<div class="main-menu f-right d-none d-lg-block">
+								<nav> 
+									<ul id="navigation">                                                                                                                   
+										<li><a href="index.html">Inicio</a></li>
+										<li><a href="about.html">Acerca de</a></li>
+										<li><a href="project.html">Proyectos</a></li>
+										<li><a href="services.html">Servicios</a></li>
+										{{-- <li><a href="blog.html">Blog</a>
+											<ul class="submenu">
+												<li><a href="blog.html">Blog</a></li>
+												<li><a href="single-blog.html">Blog Details</a></li>
+											</ul>
+										</li> --}}
+										{{-- <li><a href="#">Pages</a>
+											<ul class="submenu">
+												<li><a href="elements.html">Element</a></li>
+												<li><a href="project_details.html">Projects Details</a></li>
+												<li><a href="services_details.html">Services Details</a></li>
+											</ul>
+										</li> --}}
+										<li><a href="contact.html">Contact</a></li>
+									</ul>
+								</nav>
+							</div>
+						</div>             
+						<div class="col-xl-2 col-lg-2 col-md-3">
+							<div class="header-right-btn f-right d-none d-lg-block">
+								<a href="#" class="btn">Contact Now</a>
+							</div>
+						</div>
+						<!-- Mobile Menu -->
+						<div class="col-12">
+							<div class="mobile_menu d-block d-lg-none"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Header End -->
+</header>
