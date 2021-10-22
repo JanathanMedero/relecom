040d282c6e865e4bd1a8a3a5aadeda5c8fb5b037
JanathanMedero <medero.janathan@gmail.com>
2021-10-22 14:26:26 -0500

    Responsive view Us

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -8,7 +8,7 @@
 			<div class="row">
 				<div class="col-xl-12">
 					<div class="hero-cap pt-100">
-						<h2>sobre nosotros</h2>
+						<h2 class="title-section">sobre nosotros</h2>
 					</div>
 				</div>
 			</div>
@@ -26,13 +26,16 @@
 					</div>
 				</div>
 				<div class="col-md-12">
-					<div class="support-caption pr-50">
-						<p class="text-justify" style="font-size: 15px;">El objetivo de esta empresa, es proporcionar servicios de calidad, los cuales satisfagan algunas de las necesidades primordiales del ser humano, para su desarrollo dentro de la sociedad, en la que vivimos; comprometiéndose a brindar el mejor desempeño en los trabajos que le sean solicitados.</p>
+					<div class="support-caption pr-50 d-sm-none d-none">
+						<p class="text-justify text-paragraph" style="font-size: 15px;">El objetivo de esta empresa, es proporcionar servicios de calidad, los cuales satisfagan algunas de las necesidades primordiales del ser humano, para su desarrollo dentro de la sociedad, en la que vivimos; comprometiéndose a brindar el mejor desempeño en los trabajos que le sean solicitados.</p>
+					</div>
+					<div class="support-caption d-sm-block">
+						<p class="text-justify text-paragraph" style="font-size: 15px;">El objetivo de esta empresa, es proporcionar servicios de calidad, los cuales satisfagan algunas de las necesidades primordiales del ser humano, para su desarrollo dentro de la sociedad, en la que vivimos; comprometiéndose a brindar el mejor desempeño en los trabajos que le sean solicitados.</p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-6 container-image-photo">
 			<div class="row">
 				<div class="col-md-12">
 					<img src="{{ asset('assets/img/about/image.jpg') }}" alt="Objetivo" class="img-fluid">
@@ -42,15 +45,9 @@
 	</div>
 
 	<div class="row pt-50 pb-50">
-		<div class="col-md-6">
+
+		<div class="col-md-6 d-flex align-items-center d-sm-block">
 			<div class="row">
-				<div class="col-md-12">
-					<img src="{{ asset('assets/img/about/image2.jpg') }}" alt="Visión" class="img-fluid">
-				</div>
-			</div>
-		</div>
-		<div class="col-md-6 d-flex align-items-center">
-			<div class="row pl-50">
 				<div class="col-md-12">
 					<div class="text-title">
 						<h2 class="">Visión</h2>
@@ -63,6 +60,30 @@
 				</div>
 			</div>
 		</div>
+
+		<div class="col-md-6 container-image-photo">
+			<div class="row">
+				<div class="col-md-12">
+					<img src="{{ asset('assets/img/about/image2.jpg') }}" alt="Visión" class="img-fluid">
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-6 d-flex align-items-center">
+			<div class="row pl-50 d-none">
+				<div class="col-md-12">
+					<div class="text-title">
+						<h2 class="">Visión</h2>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<div class="support-caption">
+						<p class="text-justify" style="font-size: 15px;">Ofrecer servicios y proyectos a un mayor número de comunidades como líder, al ofrecer la mejor experiencia y satisfacer a sus necesidaddes con la mejor alidad de nuestros colaboradores, derivado de una constante capacitación e implementeción de nuevas tecnologías.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</div>
 
 	<div class="row pt-50 pb-100">
@@ -74,13 +95,16 @@
 					</div>
 				</div>
 				<div class="col-md-12">
-					<div class="support-caption pr-50">
+					<div class="support-caption pr-50 d-none">
+						<p class="text-justify" style="font-size: 15px;">Satisfacer las necesidades de redes y de servicios, fomentando en cada uno de nosotros nuestra filosofía y valores, para asegurar una relación permanente y valiosa con nuestros clientes, proveedores, accionistas, y edio ambiente, obteniendo de esta manera una adecuada rentabilidad y garantizando asi nuestra permanencia y crecimiento.</p>
+					</div>
+					<div class="support-caption d-sm-block">
 						<p class="text-justify" style="font-size: 15px;">Satisfacer las necesidades de redes y de servicios, fomentando en cada uno de nosotros nuestra filosofía y valores, para asegurar una relación permanente y valiosa con nuestros clientes, proveedores, accionistas, y edio ambiente, obteniendo de esta manera una adecuada rentabilidad y garantizando asi nuestra permanencia y crecimiento.</p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-6 container-image-photo">
 			<div class="row">
 				<div class="col-md-12">
 					<img src="{{ asset('assets/img/about/image3.jpg') }}" alt="Misión" class="img-fluid">
